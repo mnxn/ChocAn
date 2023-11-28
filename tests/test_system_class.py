02aa325d3057377502db2790d025e616e485110a
AJR <aidenjruss@gmail.com>
2023-11-28 15:28:43 -0800

    set up the file to test the system class

---
+++ tests/test_system_class.py
@@ -0,0 +1,57 @@
+import unittest
+from choc_an import system
+
+class TestSystemClass(unittest.TestCase):
+    def test_system(self):
+        pass
+
+    def test_load_files(self):
+        pass
+
+    def test_write_files(self):
+        pass
+
+    def test_add_member(self):
+        pass
+
+    def test_remove_member(self):
+        pass
+
+    def test_suspend_member(self):
+        pass
+
+    def test_lookup_member(self):
+        pass
+
+    def test_add_provider(self):
+        pass
+
+    def test_remove_provider(self):
+        pass
+
+    def test_lookup_provider(self):
+        pass
+
+    def test_lookup_manager(self):
+        pass
+
+    def test_lookup_service(self):
+        pass
+
+    def test_record_service(self):
+        pass
+
+    def test_issue_member_report(self):
+        pass
+
+    def test_issue_provider_report(self):
+        pass
+
+    def test_issue_provider_directory(self):
+        pass
+
+    def test_issue_summary_report(self):
+        pass
+
+    def test_write_eft_data(self):
+        pass
