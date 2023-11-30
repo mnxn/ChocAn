eb5c718c7066db33f349bde16ad726f6d69e5916
zhihw <wzhlephone@gmail.com>
2023-11-29 19:47:37 -0800

    Modification for readonly

--- tests/test_system_class.py
+++ tests/test_system_class.py
@@ -8,12 +8,12 @@ class TestSystemClass(unittest.TestCase):
     def test_system(self):
         nonexistent_path = "/path/that/does/not/exist"
         with self.assertRaises(FileNotFoundError) as cm:
-            system.System(nonexistent_path)
+            system.System(nonexistent_path,True)
         the_exception = cm.exception
         self.assertEqual(str(the_exception), f"The specified path does not exist: {nonexistent_path}")
         
     def test_load_files(self):
-        sys = system.System("tests") 
+        sys = system.System("tests",False) 
         with self.assertRaises(FileNotFoundError):
             sys.load_members()
 
@@ -29,18 +29,12 @@ class TestSystemClass(unittest.TestCase):
         with self.assertRaises(FileNotFoundError):
             sys.load_records()
        
-        sys2 = system.System("data")
-        manager=user.Manager("Harris Martin")
-        sys2.manager_list.append(manager)
-        self.assertEqual(len(sys2.manager_list), 1)
+        sys2 = system.System("data", False)
+        self.assertEqual(len(sys2.manager_list), 10)
         self.assertEqual(sys2.manager_list[0].name, "Harris Martin")
-        for data in sys2.manager_list:
-            if data.name == manager.name:
-                sys2.manager_list.remove(data)
-                sys2.write_files()
         #Each load function is almost the same, if one is correct then all are correct
     def test_write_files(self):
-        sys = system.System("data") 
+        sys = system.System("data",False) 
         member=user.Member("Member", 123456789, "address1", "Portland", "OR", 99999, False)
         sys.member_list.append(member)
         sys.write_files()
@@ -55,7 +49,7 @@ class TestSystemClass(unittest.TestCase):
         sys.remove_member(123456789)
         #The write_file() function just calls the write_data() function multiple times and only needs to test whether write_data() is correct, so it is enough to only test writing member.json
     def test_add_member(self):
-        sys = system.System("data") 
+        sys = system.System("data",False) 
         member=user.Member("Member2", 123456780, "address1", "Portland", "OR", 99999, False)
         sys.add_member(member)
         member_confirm=sys.lookup_member(123456780)
@@ -67,14 +61,14 @@ class TestSystemClass(unittest.TestCase):
         self.assertEqual(member.suspended, member_confirm.suspended)
 
     def test_remove_member(self):
-        sys = system.System("data") 
+        sys = system.System("data",False) 
         sys.remove_member(123456780)
         with self.assertRaises(Exception) as context:
             sys.lookup_member(123456780)
         self.assertEqual(str(context.exception), "Member not Found")
 
     def test_suspend_member(self):
-        sys = system.System("data") 
+        sys = system.System("data",False) 
         sys.suspend_member(258137067)
         member_confirm=sys.lookup_member(258137067)
         self.assertEqual(True, member_confirm.suspended)
@@ -83,7 +77,7 @@ class TestSystemClass(unittest.TestCase):
         self.assertEqual(False, member_confirm.suspended)
 
     def test_lookup_member(self):
-        current_system = System("data")
+        current_system = system.System("data",False) 
         member_confirm = current_system.lookup_member(628574130)
         known_member = user.Member(
             name ="Harris Martin",
@@ -100,7 +94,7 @@ class TestSystemClass(unittest.TestCase):
         self.assertEqual(known_member.suspended, member_confirm.suspended)
 
     def test_add_provider(self):
-        current_system = System("data")
+        current_system =system.System("data",False) 
         test_provider = user.Provider(
             name = "Austin Myers",
             id = 123456789,
@@ -119,12 +113,12 @@ class TestSystemClass(unittest.TestCase):
         self.assertEqual(test_provider.city, confirm_provider.city)
         self.assertEqual(test_provider.state, confirm_provider.state)
         self.assertEqual(test_provider.zip_code, confirm_provider.zip_code)
-        
+        current_system.remove_provider(123456789)
 
         
 
     def test_remove_provider(self):
-        current_system = System("data")
+        current_system = system.System("data",False) 
         test_provider = user.Provider(
             name = "Austin Myers",
             id = 987654321,
@@ -144,7 +138,7 @@ class TestSystemClass(unittest.TestCase):
         self.assertEqual(str(context.exception), "Provider Not Found")
 
     def test_lookup_provider(self):
-        current_system = System("data")
+        current_system = system.System("data",False) 
         provider_confirm = current_system.lookup_provider(186972363)
         known_provider = user.Provider(
             name = "Miller Nelson",
@@ -164,7 +158,7 @@ class TestSystemClass(unittest.TestCase):
         pass
 
     def test_lookup_manager(self):
-        current_system = System("data")
+        current_system = system.System("data",False) 
         manager_confirm = current_system.lookup_manager("Gaven Martin")
         known_manager = user.Manager(
             name ="Gaven Martin"
